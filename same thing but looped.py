04accc33aaa6ca6ab72de172fe55ad7d447d98ed
Thanoid <54818737+Thanoid69@users.noreply.github.com>
2022-07-09 23:03:21 +0200

    Add files via upload

---
+++ same thing but looped.py
@@ -0,0 +1,27 @@
+import mcstatus
+from mcstatus import JavaServer
+
+
+
+def looop():
+    ip = input("IP and PORT (example: 111.1.111.111:25565): ")
+
+
+    server = JavaServer.lookup(ip)
+
+    status = server.status()
+    latency =  server.ping()
+    query = server.query()
+
+    print(f"""\n{status.players.online} Players online. {status.latency} ms
+    The server has the following players online: {', '.join(query.players.names)}
+    Version: {'' .join(query.software.version)}
+    Version: {'' .join(query.software.brand)}
+    Plugins: {', ' .join(query.software.plugins)}
+    Motd: {'' .join(query.motd)}
+    Map: {'' .join(query.map)}
+    """)
+
+while 5 < 6:
+    looop()
+
