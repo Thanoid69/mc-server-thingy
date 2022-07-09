cf881e1e1958ef23c6ddbbea67dad46a283c8522
Thanoid <54818737+Thanoid69@users.noreply.github.com>
2022-07-09 23:31:09 +0200

    Update mc server thing.py

--- mc server thing.py
+++ mc server thing.py
@@ -1,23 +1,51 @@
 import mcstatus
 from mcstatus import JavaServer
 
+
+
+
 ip = input("IP and PORT (example: 111.1.111.111:25565): ")
 
+mode = input("Select Mode  (1)Query Enabled (2)Query Disabled: ")
 
 server = JavaServer.lookup(ip)
 
 status = server.status()
 latency =  server.ping()
-query = server.query()
 
-print(f"""\n{status.players.online} Players online. {status.latency} ms
-The server has the following players online: {', '.join(query.players.names)}
+
+
+
+
+def Query():
+    query = server.query()
+
+    print(f"""
+Ping {status.latency} ms
+Player Slots: {status.players.max}
+Number of Players Online: {status.players.max}/{status.players.online}
+Players Online: [{', '.join(query.players.names)}]
 Version: {'' .join(query.software.version)}
 Version: {'' .join(query.software.brand)}
-Plugins: {', ' .join(query.software.plugins)}
-Motd: {'' .join(query.motd)}
+Plugins: [{', ' .join(query.software.plugins)}]
+Motd: "{'' .join(query.motd)}"
 Map: {'' .join(query.map)}
-""")
+    """)
 
+def Normal():
+    print(f"""
+Ping {status.latency} ms
+Player Slots: {status.players.max}
+Number of Players Online: {status.players.max}/{status.players.online}
+    """)
+
+if mode ==  "1":
+    Query()
+
+elif mode == "2":
+    Normal()
+
+else:
+    print("Error, Invalid Mode.")
 
 input()
