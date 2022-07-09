8e965c50ce5465c2eef0f02ee5257b612658f285
Thanoid69 <54818737+Thanoid69@users.noreply.github.com>
2022-07-09 21:57:37 +0200

    Add files via upload

---
+++ mc server thing.py
@@ -0,0 +1,24 @@
+import mcstatus
+from mcstatus import JavaServer
+import sys
+
+ip = input("IP and PORT (example; 111.1.111.111:25565): ")
+
+
+server = JavaServer.lookup(ip)
+
+status = server.status()
+
+latency =  server.ping()
+
+query = server.query()
+
+print(f"{status.players.online} Players online. {status.latency} ms")
+print(f"The server has the following players online: {', '.join(query.players.names)}")
+print(f"Version: {'' .join(query.software.version)}")
+print(f"Version: {'' .join(query.software.brand)}")
+print(f"Plugins: {', ' .join(query.software.plugins)}")
+print(f"Motd: {'' .join(query.motd)}")
+print(f"Map: {'' .join(query.map)}")
+
+input()
