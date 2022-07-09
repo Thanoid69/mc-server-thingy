4257fcf10fb9a693bc8c67d884c14f7c2d0bdbea
Thanoid <54818737+Thanoid69@users.noreply.github.com>
2022-07-09 23:39:27 +0200

    Update mc server thing.py

--- mc server thing.py
+++ mc server thing.py
@@ -15,29 +15,33 @@ latency =  server.ping()
 
 
 
-
-
 def Query():
-    query = server.query()
+    try:    
+        query = server.query()
 
-    print(f"""
-Ping {status.latency} ms
-Player Slots: {status.players.max}
-Number of Players Online: {status.players.max}/{status.players.online}
-Players Online: [{', '.join(query.players.names)}]
-Version: {'' .join(query.software.version)}
-Version: {'' .join(query.software.brand)}
-Plugins: [{', ' .join(query.software.plugins)}]
-Motd: "{'' .join(query.motd)}"
-Map: {'' .join(query.map)}
-    """)
+        print(f"""
+    Ping {status.latency} ms
+    Player Slots: {status.players.max}
+    Number of Players Online: {status.players.max}/{status.players.online}
+    Players Online: [{', '.join(query.players.names)}]
+    Version: {'' .join(query.software.version)}
+    Version: {'' .join(query.software.brand)}
+    Plugins: [{', ' .join(query.software.plugins)}]
+    Motd: "{'' .join(query.motd)}"
+    Map: {'' .join(query.map)}
+        """)
+    except Exception:
+        print('Error, Not a Minecraft Server or has enable-query=false')
 
 def Normal():
-    print(f"""
-Ping {status.latency} ms
-Player Slots: {status.players.max}
-Number of Players Online: {status.players.max}/{status.players.online}
-    """)
+    try:
+        print(f"""
+    Ping {status.latency} ms
+    Player Slots: {status.players.max}
+    Number of Players Online: {status.players.max}/{status.players.online}
+        """)
+    except Exception:
+        print('Error, Not a Minecraft Server')
 
 if mode ==  "1":
     Query()
