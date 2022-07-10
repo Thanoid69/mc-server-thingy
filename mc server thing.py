1b799c830dbb6be2b53583db7cec085ada811dbe
Thanoid <54818737+Thanoid69@users.noreply.github.com>
2022-07-10 18:30:09 +0200

    Auto switch to less-info mode if query is disabled.
    everything is in the title

--- mc server thing.py
+++ mc server thing.py
@@ -7,7 +7,6 @@ print("! This tool is for version 1.7 and up !\n")
 
 ip = input("IP and PORT (example: 111.1.111.111:25565): ")
 
-mode = input("Select Mode  (1)Query Enabled (2)Query Disabled: ")
 
 server = JavaServer.lookup(ip)
 
@@ -16,41 +15,39 @@ latency =  server.ping()
 
 
 
-def Query():
+def lol():
     try:    
         query = server.query()
 
         print(f"""
+[    
     Ping {status.latency} ms
-    Player Slots: {status.players.max}
-    Number of Players Online: {status.players.max}/{status.players.online}
-    Players Online: [{', '.join(query.players.names)}]
+    Player slots: {status.players.max}
+    Number of players online: {status.players.max}/{status.players.online}
+    Players online: [{', '.join(query.players.names)}]
     Version: {'' .join(query.software.version)}
     Version: {'' .join(query.software.brand)}
     Plugins: [{', ' .join(query.software.plugins)}]
     Motd: {'' .join(query.motd)}"
     Map: {'' .join(query.map)}
+[
         """)
     except Exception:
-        print('Error, Not a Minecraft Server or has enable-query=false')
-
-def Normal():
-    try:
-        print(f"""
+           
+            try:
+                print(f"""
+[        
     Ping {status.latency} ms
-    Player Slots: {status.players.max}
-    Number of Players Online: {status.players.max}/{status.players.online}
-        """)
-    except Exception:
-        print('Error, Not a Minecraft Server')
+     Player slots: {status.players.max}
+    Number of Players online: {status.players.max}/{status.players.online}
+[
+                """)
 
-if mode ==  "1":
-    Query()
+            except Exception:
+                print('Error, not a Minecraft server')
+            
+            print('Less info because the server has enable-query=false')
 
-elif mode == "2":
-    Normal()
-
-else:
-    print("Error, Invalid Mode.")
+lol()
 
 input()
